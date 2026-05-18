068a509fcbc48dbbd682ad5775e3f5dbc30cf2b1
Lynx <lynx@ilynxcat.me>
2026-05-18 17:42:25 -0500

    add camera shutter sfx

--- src/assets.rs
+++ src/assets.rs
@@ -1,14 +1,20 @@
-use raylib::{RaylibHandle, RaylibThread, texture::Texture2D};
+use raylib::{
+    RaylibHandle, RaylibThread,
+    audio::{RaylibAudio, Sound},
+    texture::Texture2D,
+};
 
-pub struct GameAssets {
+pub struct GameAssets<'aud> {
     pub player_sprite: Texture2D,
     pub crosshair_sprite: Texture2D,
     pub title_sprite: Texture2D,
     pub texture_atlas: Texture2D,
+
+    pub camera_shutter: Sound<'aud>,
 }
 
-impl GameAssets {
-    pub fn load(rl: &mut RaylibHandle, thread: &RaylibThread) -> Self {
+impl<'aud> GameAssets<'aud> {
+    pub fn load(rl: &mut RaylibHandle, thread: &RaylibThread, audio: &'aud RaylibAudio) -> Self {
         Self {
             player_sprite: rl
                 .load_texture(thread, "res/bob.png")
@@ -22,6 +28,10 @@ impl GameAssets {
             texture_atlas: rl
                 .load_texture(thread, "res/atlas.png")
                 .expect("Failed to load texture atlas"),
+
+            camera_shutter: audio
+                .new_sound("res/sfx/shutter.mp3")
+                .expect("Failed to load camera shutter sound"),
         }
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -3,22 +3,22 @@ use crate::{
     level::Level,
     scene::{Scene, world::WorldScene},
 };
-use raylib::{RaylibHandle, RaylibThread};
+use raylib::{RaylibHandle, RaylibThread, audio::RaylibAudio};
 
-pub struct GameState {
+pub struct GameState<'aud> {
     pub scene: Scene,
-    pub assets: GameAssets,
+    pub assets: GameAssets<'aud>,
 
     pub is_music_paused: bool,
 }
 
-impl GameState {
-    pub fn load(rl: &mut RaylibHandle, thread: &RaylibThread) -> Self {
+impl<'aud> GameState<'aud> {
+    pub fn load(rl: &mut RaylibHandle, thread: &RaylibThread, audio: &'aud RaylibAudio) -> Self {
         let level = Level::new();
 
         Self {
             scene: Scene::World(WorldScene::new(level)),
-            assets: GameAssets::load(rl, thread),
+            assets: GameAssets::load(rl, thread, audio),
 
             is_music_paused: cfg!(debug_assertions),
         }
