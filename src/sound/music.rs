f40465516ce18898ce6e7f7ea800fd273e42452b
Lynx <lynx@ilynxcat.me>
2026-05-18 20:23:13 -0500

    music: re-add summer night feast

--- src/sound/music.rs
+++ src/sound/music.rs
@@ -2,6 +2,7 @@ use raylib::audio::{Music, RaylibAudio};
 
 pub struct MusicTracks<'aud> {
     pub lamentable: Music<'aud>,
+    pub summer_night_feast: Music<'aud>,
 }
 
 impl<'aud> MusicTracks<'aud> {
@@ -10,6 +11,9 @@ impl<'aud> MusicTracks<'aud> {
             lamentable: audio
                 .new_music("res/music/lamentable.mp3")
                 .expect("Failed to load music/lamentable.mp3"),
+            summer_night_feast: audio
+                .new_music("res/music/summer-night-feast.mp3")
+                .expect("Failed to load music/summer-night-feast.mp3"),
         }
     }
 }
