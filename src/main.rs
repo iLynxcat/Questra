a2ab0c047936fc9982074189428a46d1802ac83e
Lynx <lynx@ilynxcat.me>
2026-07-09 15:12:36 -0500

    strip old code, set up bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,24 @@
-use questra::{
-    scene::{Scene, transition::SceneTransition},
-    sound::init_audio,
-    state::GameState,
-};
-use raylib::{audio::Music, color::Color, drawing::RaylibDraw, ffi::KeyboardKey};
+use bevy::prelude::*;
 
 const TITLE: &str = concat!("Questra Alpha ", env!("CARGO_PKG_VERSION_PATCH"));
 
+fn hi() {
+    println!("HAI");
+}
+
 fn main() {
-    let (mut rl, thread) = raylib::init() //
-        .size(640, 480)
-        .title(TITLE)
-        .resizable()
-        .build();
-
-    rl.set_target_fps(60);
-    rl.set_exit_key(None);
-
-    let audio = init_audio(1.0);
-    let mut state = GameState::load(&mut rl, &thread, &audio);
-
-    let ambience_tracks: Vec<&Music<'_>> = vec![
-        &state.assets.music.lamentable,
-        &state.assets.music.summer_night_feast,
-    ];
-    let mut ambience_i: usize = if cfg!(debug_assertions) {
-        // muted by default, so unmuting triggers the correct track to play
-        0
-    } else {
-        // start at the end of the list so our handler automatically wraps
-        ambience_tracks.len() - 1
-    };
-
-    while !rl.window_should_close() {
-        if rl.is_key_pressed(KeyboardKey::KEY_M) || rl.is_key_pressed_repeat(KeyboardKey::KEY_M) {
-            state.is_music_paused = !state.is_music_paused;
-            ambience_tracks[ambience_i].play_stream();
-        }
-        if rl.is_key_pressed(KeyboardKey::KEY_N) || rl.is_key_pressed_repeat(KeyboardKey::KEY_N) {
-            ambience_tracks[ambience_i].stop_stream();
-            advance_track(&mut ambience_i, ambience_tracks.len());
-            ambience_tracks[ambience_i].play_stream();
-        }
-
-        let mut d = rl.begin_drawing(&thread);
-
-        let transition = match &mut state.scene {
-            Scene::Title(scene) => scene.update(&d, &state.assets),
-            Scene::World(scene) => scene.update(&d, &state.assets),
-        };
-        match transition {
-            SceneTransition::To(next) => {
-                state.scene = next;
-            }
-            SceneTransition::Quit => break,
-            SceneTransition::None => {}
-        };
-
-        match &mut state.scene {
-            Scene::Title(scene) => scene.draw(&mut d, &state.assets),
-            Scene::World(scene) => scene.draw(&mut d, &state.assets),
-        };
-
-        let track = &ambience_tracks[ambience_i];
-        if state.is_music_paused {
-            d.draw_text(
-                "Music Paused",
-                10,
-                d.get_screen_height() - 20,
-                18,
-                Color::GOLDENROD,
-            );
-
-            if track.is_stream_playing() {
-                track.pause_stream();
-            }
-
-            track.update_stream();
-        } else {
-            if !track.is_stream_playing() {
-                advance_track(&mut ambience_i, ambience_tracks.len());
-                let track = ambience_tracks[ambience_i];
-                track.play_stream();
-                track.update_stream();
-            } else {
-                track.update_stream();
-            }
-        }
-    }
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: TITLE.to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Update, hi)
+        .run();
 }
 
-fn advance_track(index: &mut usize, track_count: usize) {
-    *index = (*index + 1) % track_count
-}
+// fn advance_track(index: &mut usize, track_count: usize) {
+//     *index = (*index + 1) % track_count
+// }
